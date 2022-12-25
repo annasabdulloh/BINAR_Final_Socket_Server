149127fc791a5282e9ba5b9fe0be8d2813201930
Ahmad Syaifudin Ramadhan <53831549+SyaifudinRamadhan@users.noreply.github.com>
2022-12-25 23:40:16 +0700

    add cors 1

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const { Server } = require("socket.io");
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, cors: {
+    origin: '*',
+  });
 
 global.io = io
 
